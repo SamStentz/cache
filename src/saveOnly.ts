0cde73b356c2b2315435d004886f16db8565fe31
Sam Stentz <samstentz@google.com>
2023-11-28 20:38:57 +0000

    added save only

--- src/saveOnly.ts
+++ src/saveOnly.ts
@@ -1,10 +1,9 @@
 import * as core from "@actions/core";
 
 import saveImpl from "./saveImpl";
-import { NullStateProvider } from "./stateProvider";
 
 async function run(): Promise<void> {
-    const cacheId = await saveImpl(new NullStateProvider());
+    const cacheId = await saveImpl();
     if (cacheId === -1) {
         core.warning(`Cache save failed.`);
     }
